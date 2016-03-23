65edbc6644fe4b5393534af565d3ed26fc186a6c
Len Lorijn <len.lorijn@mediact.nl>
2016-03-23 20:57:39 +0100

    feh to basic software

--- install-scripts/debian/basic-software.sh
+++ install-scripts/debian/basic-software.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "\nInstalling basic software\n"
-sudo apt-get install -y git git-core htop rar unrar iw weechat-curses vim wget curl terminator
+sudo apt-get install -y git git-core htop rar unrar iw weechat-curses vim wget curl terminator feh
 curl https://bootstrap.pypa.io/get-pip.py | sudo python
 echo "\nInstalled basic software\n"
