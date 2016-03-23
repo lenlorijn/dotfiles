6e5a06a328098d39eaee3b88b2a7d47fdcb8c94d
Len Lorijn <len.lorijn@mediact.nl>
2016-03-23 18:49:12 +0100

    remove firefox from basic software

--- install-scripts/debian/basic-software.sh
+++ install-scripts/debian/basic-software.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "\nInstalling basic software\n"
-sudo apt-get install -y git git-core htop rar unrar firefox iw weechat-curses vim python-pip wget curl terminator
+sudo apt-get install -y git git-core htop rar unrar iw weechat-curses vim python-pip wget curl terminator
 echo "\nInstalled basic software\n"
