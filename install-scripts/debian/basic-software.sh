5fc8fa1ad419c6c5dbb336aea90d8ca4f712cae0
Len Lorijn <len.lorijn@mediact.nl>
2016-03-23 18:47:17 +0100

    add terminator to basic software

--- install-scripts/debian/basic-software.sh
+++ install-scripts/debian/basic-software.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "\nInstalling basic software\n"
-sudo apt-get install -y git git-core htop rar unrar firefox iw weechat-curses vim python-pip wget curl
+sudo apt-get install -y git git-core htop rar unrar firefox iw weechat-curses vim python-pip wget curl terminator
 echo "\nInstalled basic software\n"
